cf463b688f6a4e868a1a2db99bc08f6767a660ed
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-05-04 16:52:07 +0530

    Pascal Triangle

---
+++ Arrays/Kth Row of Pascal's Triangle.js
@@ -0,0 +1,29 @@
+function pascal(A) {
+  var array = [[1], [1, 1]];
+  var v = 0;
+  var temp = [];
+
+  if (A === 0) {
+    return;
+  } else if (A === 1) {
+    return [[1]];
+  } else if (A === 2) {
+    return [[1], [1, 1]];
+  } else if (A > 2) {
+    for (var i = 2; i < A; i++) {
+      for (var j = 0; j < i + 1; j++) {
+        if (j === 0 || j === i) {
+          temp.push(1);
+        } else {
+          v = array[i - 1][j] + array[i - 1][j - 1];
+          temp.push(v);
+        }
+      }
+      array[i] = temp;
+      temp = [];
+    }
+    return array;
+  }
+}
+
+console.log(pascal(0));
